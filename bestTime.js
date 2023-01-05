0049ffdf63d961a3ac273d4d26eda62a677c92d9
Timi <alabitimi65@gmail.com>
2023-01-04 23:20:56 -0700

    first attempt

---
+++ bestTime.js
@@ -0,0 +1,13 @@
+let prices = [7, 1, 5, 3, 6, 4];
+
+let maxProfit = function (prices) {
+  for (i = 0; i < prices.length; i++) {
+    let price = prices[i];
+  }
+};
+let maxPrice = function (prices) {
+  return Math.max(...prices);
+};
+
+console.log(maxPrice(prices));
+console.log(maxProfit(prices));
