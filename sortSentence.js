828db10a2fd4e17c4315c852e4af5b650bf800b5
Timi <alabitimi65@gmail.com>
2024-01-05 23:08:18 -0700

    minor update to sortSentence

--- sortSentence.js
+++ sortSentence.js
@@ -6,18 +6,30 @@ function isolateInteger(str) {
 }
 
 const sortSentence = function (s) {
-  let sortedS = [];
+  //   let sortedS = [];
   let sArr = [];
 
   sArr = s.split(" ");
 
-  for (let i = 0; i < sArr.length; i++) {
-    if (/\d/.test(sArr[i])) {
-      let index = isolateInteger(sArr[i]);
-      sArr.sort(index);
-      console.log(sArr);
-    }
+  //   for (let i = 0; i < sArr.length; i++) {
+  //     if (/\d/.test(sArr[i])) {
+  //       let index = isolateInteger(sArr[i]);
+  //       sArr.sort(index);
+  //       console.log(sArr);
+  //     }
+  //   }
+
+  function compareByNumber(a, b) {
+    const numberA = parseInt(a.match(/\d+/)[0], 10);
+    const numberB = parseInt(b.match(/\d+/)[0], 10);
+
+    return numberA - numberB;
   }
+
+  sArr.sort(compareByNumber);
+  return sArr;
 };
 
 console.log(sortSentence(s));
+
+// Output: ["This", "1", "is", "2", "sentence", "
