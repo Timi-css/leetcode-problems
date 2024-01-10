b6dd43984e4f4061af39ab83694f7d793a99eb1a
Timi <alabitimi65@gmail.com>
2024-01-09 21:59:14 -0700

    update to code to complete solution

--- almostEqual.js
+++ almostEqual.js
@@ -4,7 +4,7 @@ Problem: Given two strings of equal length, swap the indices/character of a stri
 Example: s1 = "bank", s2 = "kanb". Swap s2[0] and s2[3] to produce the string s1. Output will be true
 */
 
-let s1 = "attack";
+let s1 = "bank";
 let s2 = "defend";
 
 function areAlmostequal(s1, s2) {
@@ -15,18 +15,32 @@ function areAlmostequal(s1, s2) {
   if (s1 === s2) return true + " Strings are already equal";
   //   Check if the arrays are the same length
   if (s1Arr.length !== s2Arr.length) {
+    console.log("Length not equal");
     return false;
   }
 
+  function swapElements(arr, index1, index2) {
+    // Check if indices are valid
+    if (
+      index1 >= 0 &&
+      index1 < arr.length &&
+      index2 >= 0 &&
+      index2 < arr.length
+    ) {
+      // Swap elements using a temporary variable
+      var temp = arr[index1];
+      arr[index1] = arr[index2];
+      arr[index2] = temp;
+    }
+  }
   //   Loop thru s2Arr and swap first index for the last
   for (let i = 0; i < s2Arr.length; i++) {
-    console.log(s2Arr[i]);
-    let firstIndex = s2Arr[0];
-    let lastIndex = s2Arr[3];
-    if (firstIndex === lastIndex && firstIndex !== s1Arr[i]) return false;
-    if (s1Arr[0] !== s2Arr[i - 1]) return false;
+    let firstIndex = 0;
+    let lastIndex = s2Arr.length - 1;
+    swapElements(s2Arr, firstIndex, lastIndex);
+    console.log(s2Arr);
+    return true;
   }
-  return true;
 }
 
 console.log(areAlmostequal(s1, s2));
