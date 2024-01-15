79baedb8cdaf1316c2abe26075c112231a15f751
Timi <alabitimi65@gmail.com>
2024-01-14 23:56:53 -0700

    added comments/explanation

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,10 +1,13 @@
 let nums = [1, 1, 2];
 
 const removeDuplicates = function (nums) {
+  // Define an empty array to push result
   let expectedNums = [];
+  //   sort the input array
   let sortedNums = nums.sort();
-
+  //  loop through to isolate indeces
   for (i = 0; i < sortedNums.length; i++) {
+    // check for index existence in the result array
     if (!expectedNums.includes(sortedNums[i])) {
       expectedNums.push(sortedNums[i]);
     }
