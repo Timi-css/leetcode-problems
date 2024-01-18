554f7d84cbbbc99c80d4e4675c1b3cf5046878c6
Timi <alabitimi65@gmail.com>
2024-01-17 23:40:43 -0700

    refactoring the code for bette solution

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,18 +1,30 @@
-let nums = [1, 1, 2];
+let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 
 const removeDuplicates = function (nums) {
   // Define an empty array to push result
-  let expectedNums = [];
+  //   let expectedNums = [];
   //   sort the input array
-  let sortedNums = nums.sort();
-  //  loop through to isolate indeces
-  for (i = 0; i < sortedNums.length; i++) {
-    // check for index existence in the result array
-    if (!expectedNums.includes(sortedNums[i])) {
-      expectedNums.push(sortedNums[i]);
+  //   let sortedNums = nums.sort();
+  //   //  loop through to isolate indeces
+  //   for (i = 0; i < sortedNums.length; i++) {
+  //     // check for index existence
+  //     if (!expectedNums.includes(sortedNums[i])) {
+  //       expectedNums.push(sortedNums[i]);
+  //     }
+  //   }
+  //   return expectedNums;
+  left = 0;
+  right = 1;
+
+  while (right < nums.length) {
+    if (nums[left] == nums[right]) {
+      right++;
+    } else {
+      left++;
+      nums[left] = nums[right];
     }
   }
-  return expectedNums;
+  return nums;
 };
 
 console.log(removeDuplicates(nums));
